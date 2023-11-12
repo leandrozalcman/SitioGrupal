53faac9a34de8592ef1d97a379a358acd6ef8c25
leandrozalcman <142274722+leandrozalcman@users.noreply.github.com>
2023-11-12 16:10:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function toggleDarkMode() {
+    var body = document.getElementsByTagName("body")[0];
+    var textBox = document.querySelector(".text-box");
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+        textBox.style.color = "white";
+        document.querySelector(".background-image").style.backgroundImage = "url('https://img.freepik.com/free-vector/dark-green-abstract-background_1340-17018.jpg?w=1480&t=st=1699752554~exp=1699753154~hmac=1658351802d9143334df6c74993e01a13ecf2d6a787a29fce6d36c42653795bb')";
+        localStorage.setItem("theme", "dark");
+    } else {
+        textBox.style.color = "black";
+        document.querySelector(".background-image").style.backgroundImage = "url('https://img.freepik.com/free-vector/green-curve-frame-template-vector_53876-113965.jpg?w=1380&t=st=1698963501~exp=1698964101~hmac=03e822a69934e394bc40c6fe46bb1b25a2aa68cb7e6c76e464105a636e129cdc')";
+        localStorage.setItem("theme", "light");
+    }
+    var darkModeButton = document.querySelector(".dark-mode");
+    if (body.classList.contains("dark")) {
+        darkModeButton.innerHTML = '<i class="fas fa-moon"></i>';
+    } else {
+        darkModeButton.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+}
+
+function toggleMenu() {
+    var x = document.getElementsByClassName("menu")[0];
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+
+window.onload = function () {
+    var theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.getElementsByTagName("body")[0].classList.add("dark");
+        document.querySelector(".text-box").style.color = "white";
+        document.querySelector(".background-image").style.backgroundImage = "url('https://img.freepik.com/free-vector/dark-green-abstract-background_1340-17018.jpg?w=1480&t=st=1699752554~exp=1699753154~hmac=1658351802d9143334df6c74993e01a13ecf2d6a787a29fce6d36c42653795bb')";
+        document.querySelector(".dark-mode").innerHTML = '<i class="fas fa-moon"></i>';
+    }
+};
